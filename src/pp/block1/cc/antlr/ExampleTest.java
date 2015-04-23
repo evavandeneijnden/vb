293e287e4035616e44e1f079b1f8f667faf03ca1
Antoine Veenstra <antoineveenstra+git@gmail.com>
2015-04-23 14:45:15 +0200

    revert exampletest

--- src/pp/block1/cc/antlr/ExampleTest.java
+++ src/pp/block1/cc/antlr/ExampleTest.java
@@ -18,18 +18,24 @@ public class ExampleTest {
 	public void failingAcceptanceTest() {
 		// spaces in keywords are not in the rules, so claiming it's correct
 		// will fail
-		tester.wrong("whi le do");
+		tester.correct("whi le do");
 	}
 
 	@Test
 	public void failingYieldCount() {
 		// the sequence consists of three dos, not two
-		tester.yields("do dodo", Example.DO, Example.DO, Example.DO);
+		tester.yields("do dodo", Example.DO, Example.DO);
+	}
+
+	@Test
+	public void failingYieldToken() {
+		// the second token is a DO not a WHILE
+		tester.yields("do dodo", Example.DO, Example.WHILE, Example.DO);
 	}
 
 	@Test
 	public void failingRejectionTest() {
 		// the following is perfectly fine, so claiming it's wrong will fail
-		tester.correct("whilewhiledodo");
+		tester.wrong("whilewhiledodo");
 	}
 }
