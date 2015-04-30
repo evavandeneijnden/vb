fefc0c6cf638c8d1c3ab16a3adc1f082d4c6e308
Eva <evavandeneijnden@gmail.com>
2015-04-30 11:06:49 +0200

    dhs fgr

--- src/pp/block2/cc/ll/MyLLCalc.java
+++ src/pp/block2/cc/ll/MyLLCalc.java
@@ -4,6 +4,8 @@ import pp.block2.cc.NonTerm;
 import pp.block2.cc.Symbol;
 import pp.block2.cc.Term;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,11 +13,24 @@ import java.util.Set;
  * Created by antoine on 29/04/15.
  */
 public class MyLLCalc implements LLCalc {
+    private Grammar grammar;
+
     public MyLLCalc(Grammar g) {
+        grammar = g;
     }
 
     @Override
     public Map<Symbol, Set<Term>> getFirst() {
+        Map<Symbol, Set<Term>> first = new HashMap<>();
+        for (Term t : grammar.getTerminals()) {
+            System.out.println(t);
+            Set<Term> temp = new HashSet<>();
+            temp.add(t);
+            first.put(t, temp);
+        }
+//        Term eof = new Term();
+
+
         return null;
     }
 
