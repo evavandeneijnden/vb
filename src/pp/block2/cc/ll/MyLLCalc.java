95149d8332bc081b467761fa60a076129c3f60af
Eva <evavandeneijnden@gmail.com>
2015-05-01 14:47:39 +0200

    Implementef "FOLLOW",fixed first, started firstp

--- src/pp/block2/cc/ll/MyLLCalc.java
+++ src/pp/block2/cc/ll/MyLLCalc.java
@@ -22,7 +22,7 @@ public class MyLLCalc implements LLCalc {
     @Override
     public Map<Symbol, Set<Term>> getFirst() {
         Map<Symbol, Set<Term>> first = new HashMap<>();
-        int iterationsSinceChange = 0;
+        boolean hasChanged = true;
 
 
         for (Term t : grammar.getTerminals()) {
@@ -32,51 +32,96 @@ public class MyLLCalc implements LLCalc {
             first.put(t, temp);
         }
 
-        for (NonTerm nt: grammar.getNonterminals()){
-            Set<Term> temp = new HashSet<>();
-            first.put(nt,temp);
+        for (NonTerm nt : grammar.getNonterminals()) {
+            first.put(nt, new HashSet<Term>());
         }
-        while (iterationsSinceChange < first.size()){
-            for (Rule r : grammar.getRules()){
+
+        while (hasChanged) {
+            hasChanged = false;
+            for (Rule r : grammar.getRules()) {
                 Set<Term> rhs = new HashSet<>();
                 int i = -1;
                 int k = r.getRHS().size();
-                if (k > 1){
+                if (k > 1) {
                     rhs = first.get(r.getRHS().get(0));
                     i = 0;
 
-                    while (first.get(r.getRHS().get(i)).contains(Symbol.EMPTY) && i<k-1){
-                        rhs.addAll(first.get(r.getRHS().get(i+1)));
-                        if (rhs.contains(Symbol.EMPTY)){
+                    while (first.get(r.getRHS().get(i)).contains(Symbol.EMPTY) && i < k - 1) {
+                        int temp = rhs.size();
+                        rhs.addAll(first.get(r.getRHS().get(i + 1)));
+                        hasChanged |= temp != rhs.size();
+                        if (rhs.contains(Symbol.EMPTY)) {
                             rhs.remove(Symbol.EMPTY);
                         }
                         i++;
                     }
-                }
-                else {
-                    if (first.get(r.getRHS().get(k)).contains(Symbol.EMPTY) && i+1==k){
+                } else {
+                    if (first.get(r.getRHS().get(k)).contains(Symbol.EMPTY) && i + 1 == k) {
                         rhs.add(Symbol.EMPTY);
                     }
                 }
             }
-
-
-            iterationsSinceChange ++;
         }
-
-
-
-        return null;
+        return first;
     }
 
     @Override
     public Map<NonTerm, Set<Term>> getFollow() {
-        return null;
+        Map<Symbol, Set<Term>> first = getFirst();
+        Map<NonTerm, Set<Term>> follow = new HashMap<>();
+        boolean hasChanged = true;
+
+        for (NonTerm nt : grammar.getNonterminals()) {
+            follow.put(nt, new HashSet<Term>());
+        }
+
+        follow.get(grammar.getStart()).add(Symbol.EOF);
+
+        while (hasChanged) {
+            hasChanged = false;
+            Set<Term> trailer = new HashSet<>();
+            int k = follow.size();
+            for (Rule r : grammar.getRules()) {
+
+                trailer = follow.get(r.getLHS());
+                for (int i = k; 0 <= i; i--) {
+                    Symbol b = r.getRHS().get(i);
+                    if (b instanceof NonTerm) {
+                        int temp = follow.get(b).size();
+                        follow.get(b).addAll(trailer);
+                        hasChanged |= temp == follow.get(b).size();
+
+                        Set<Term> bFirst = new HashSet<>(first.get(b));
+                        if (bFirst.contains(Symbol.EMPTY)) {
+                            bFirst.remove(Symbol.EMPTY);
+                            trailer.addAll(bFirst);
+                        } else {
+                            trailer = bFirst;
+                        }
+                    } else {
+                        trailer = new HashSet<>(first.get(b));
+                    }
+                }
+
+            }
+        }
+        return follow;
     }
 
     @Override
     public Map<Rule, Set<Term>> getFirstp() {
-        return null;
+        Map<NonTerm, Set<Term>> follow = getFollow();
+        Map<Rule,Set<Term>> firstP = new HashMap<>();
+
+        for (Rule r: grammar.getRules()){
+            if (r.getRHS().contains(Symbol.EMPTY)){
+                Set<Symbol> newRHS = new HashSet<>(r.getRHS());
+                newRHS.addAll(follow.get(r.getLHS()));
+                firstP.put(r,newRHS);
+            }
+        }
+
+        return getFirstp();
     }
 
     @Override
