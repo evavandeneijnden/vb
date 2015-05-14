97a1226a70be87d3825e7796432c5b0a414ba8e2
Antoine Veenstra <antoineveenstra+git@gmail.com>
2015-05-14 15:47:13 +0200

    block 4 derin

--- src/pp/block3/cc/antlr/CC5/TypeDeterminer.java
+++ src/pp/block3/cc/antlr/CC5/TypeDeterminer.java
@@ -1,10 +1,7 @@
 package pp.block3.cc.antlr.CC5;
 
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.NotNull;
-import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
-import org.antlr.v4.runtime.tree.TerminalNode;
 import pp.block3.cc.antlr.Type;
 
 /**
